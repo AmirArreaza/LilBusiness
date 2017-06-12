66d7980a9fabee1c8cbdc7a5f46a03de88b09531
AmirArreaza <Ajaarreazav@gmail.com>
2017-06-12 22:33:37 +0100

    Creation of project structure by using express-mvc framework. Login and Singup default modules added

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/companyModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');   
+  
+var routes = require('./api/routes/companyRoutes');
+routes(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
   
 app.listen(port);
+
+app._router.stack.forEach(function(r){
+  if (r.route && r.route.path){
+    console.log(r.route.path)
+  }
+})
 
 console.log('LilBusiness RESTful API server started on: ' + port);
